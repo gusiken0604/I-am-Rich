dcfef7a4e68e6c130b32f0c3a9449ec823760403
Gusiken <yassii_g@hotmail.com>
2022-10-11 15:49:01 +0900

    1

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 具志堅 on 2022/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
